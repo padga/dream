aaf04c7d1b0bb4865b1c10ad1f57b16f8a453dd1
Patrycja Nowak <nowak@host.com>
2018-04-16 23:55:37 +0200

    4 commit

--- dreams/views.py
+++ dreams/views.py
@@ -435,29 +435,3 @@ def zmien_dane(idUsers):
 def oserwisie():
     return render_template('/guest/oserwisie.html')
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
